bb95bbad0110fcec7e75bc5f38570028692a8dd0
VsevolodMurga <ipz233_mvr@student.ztu.edu.ua>
2025-02-14 13:51:39 +0200

    Update Program.cs

--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введіть перше число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Сума: {num1 + num2}");
+    }
+}
